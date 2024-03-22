6f2eb41c2fcf9512717cc634a323d36e299f36b9
MinaMalaka <153759949+MinaMalaka@users.noreply.github.com>
2024-03-22 18:43:38 +0200

    Update Products.jsx

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -67,7 +67,7 @@ function Products() {
                 </Link>
                 <div className='d-flex'>
                   <button onClick={() => addtCart(product.id)} className='btn btn-success bg-main text-white text-center w-100'>Add To Cart</button>
-                  <button className='btn' onClick={() => addWishlist(product.id);handleClick()}}><i className="heart fa-solid fa-heart"></i></button>
+                  <button className='btn' onClick={() => addWishlist(product.id);handleClick()}}><i className="heart fa-solid fa-heart fs-3"></i></button>
                 </div>
               </div>
             </div>
